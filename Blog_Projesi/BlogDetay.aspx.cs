0257dafa9e708056a74a9eb630e84a6946b7996d
Your Name <you@example.com>
2021-03-12 16:11:12 +0300

    first commit

---
+++ Blog_Projesi/BlogDetay.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Blog_Projesi.Entity;
+
+namespace Blog_Projesi
+{
+    public partial class BlogDetay : System.Web.UI.Page
+    {
+       BlogEntities db = new BlogEntities();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(Request.QueryString["BLOGID"]);
+            var blog = db.TBLBLOG.Where(x => x.BLOGID == id).ToList() ;
+            Repeater1.DataSource = blog;
+            Repeater1.DataBind();
+
+            var yorumlar = db.TBLYORUM.Where(x => x.YORUMBLOG == id).ToList();
+            Repreater2.DataSource = yorumlar;
+            Repreater2.DataBind();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(Request.QueryString["BLOGID"]);
+            TBLYORUM t = new TBLYORUM();
+            t.KULLANICIAD = TextBox1.Text;
+            t.MAIL = TextBox2.Text;
+            t.YORUMICERIK = TextBox3.Text;
+            t.YORUMBLOG = id;
+            db.TBLYORUM.Add(t);
+            db.SaveChanges();
+            Response.Redirect("BlogDetay.Aspx?BLOGID="+id);
+        }
+    }
+}
---
+++ Blog_Projesi/Default.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Blog_Projesi.Entity;
+namespace Blog_Projesi
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        BlogEntities db = new BlogEntities();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var bloglar = db.TBLBLOG.ToList();
+            Repeater1.DataSource = bloglar;
+            Repeater1.DataBind();
+
+            var bloglar2 = db.TBLKATEGORI.ToList();
+            Repeater2.DataSource = bloglar2;
+            Repeater2.DataBind();
+
+            var bloglar3 = db.TBLBLOG.ToList();
+            Repeater3.DataSource = bloglar3;
+            Repeater3.DataBind();
+        }
+    }
+}
