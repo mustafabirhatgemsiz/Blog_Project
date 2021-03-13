2818a532f80edce38e793e3ffed80bfd060da8a4
Your Name <you@example.com>
2021-03-13 18:49:20 +0300

    “2”

---
+++ Blog_Projesi/Hakkimda.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Blog_Projesi.Entity;
+
+namespace Blog_Projesi
+{
+    public partial class Hakkimda : System.Web.UI.Page
+    {
+        BlogEntities db = new BlogEntities();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Repeater1.DataSource = db.TBLHAKKIMIZDA.ToList();
+            Repeater1.DataBind();
+        }
+    }
+}
